d0d044f628f37605bb57b97689097aa931c28e92
Bárbara Lourenço Tavares <babisolua@gmail.com>
2025-04-17 21:49:30 -0300

    :sparkles:	feat: add controlador de listar sabores de marujinho

---
+++ src/controladores/sabores/listarSabores.js
@@ -0,0 +1,13 @@
+const knex = require('../../bancoDeDados/config');
+
+const listarSabores = async (req, res) => {
+    try {
+        const sabores = await knex('sabores').where({ disponivel: true }).orderBy('nome');
+
+        return res.status(200).json(sabores)
+    } catch (error) {
+        return res.status(500).json({ mensagem: 'Error interno do servidor' })
+    }
+}
+
+module.exports = listarSabores;
