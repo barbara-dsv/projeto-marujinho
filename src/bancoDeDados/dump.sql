46acdd53b3796f2f21e28cf44eeeb5edcbb5ff83
Bárbara Lourenço Tavares <babisolua@gmail.com>
2025-04-17 21:42:38 -0300

    :sparkles:	feat: add dump das tabelas do banco de dados

---
+++ src/bancoDeDados/dump.sql
@@ -0,0 +1,37 @@
+create table usuarios (
+   id serial primary key,
+    nome text not null, 
+    email text unique not null,
+    senha text not null
+);
+
+
+
+create table sabores (
+    id serial primary key, 
+    nome text not null, 
+    descricao text, 
+    preco integer not null, 
+    imagem_url text, 
+    quant_estoque integer default 0, 
+    disponivel boolean default true
+);
+
+create table pedidos(
+    id serial primary key, 
+    cep text not null, 
+    valor_total integer not null, 
+    data_pedido timestamp default now(), 
+    mensagem_whatsapp text
+); 
+ 
+create table itens_pedido(
+     id serial primary key, 
+     pedido_id integer not null references pedidos(id),
+     sabor_id integer not null references sabores(id), 
+     quantidade integer not null , 
+     preco_unitario integer not null 
+);
+
+
+
