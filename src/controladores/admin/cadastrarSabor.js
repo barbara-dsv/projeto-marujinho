cac1a8fdd0a4f0a382f5e9e6f45e2da9c9467684
Bárbara Lourenço Tavares <babisolua@gmail.com>
2025-05-26 17:10:43 -0300

    :sparkles:	feat: add rota e schema de validação de cadastrar sabor

---
+++ src/controladores/admin/cadastrarSabor.js
@@ -0,0 +1,30 @@
+const knex = require('../../bancoDeDados/config');
+
+const cadastrarSabor = async (req, res) => {
+    const { nome, descricao, preco, imagem_url, quant_estoque } = req.body;
+
+    try {
+        const saborExiste = await knex('sabores').where({ nome }).first();
+        if (saborExiste) {
+            return res.status(400).json({
+                mensagem: 'Sabor já cadastrado.'
+            })
+        }
+
+        const novoSabor = await knex('sabores').insert({
+            nome,
+            descricao,
+            preco,
+            imagem_url,
+            quant_estoque,
+            disponivel: true
+        }).returning('*')
+
+        return res.status(201).json(novoSabor)
+
+    } catch (error) {
+        return res.status(500).json({ mensagem: 'Erro do servidor' })
+    }
+}
+
+module.exports = cadastrarSabor;
---
+++ src/schemas/schemaSabor.js
@@ -0,0 +1,31 @@
+const joi = require('joi');
+
+const schemaSabor = joi.object({
+    nome: joi.string().pattern(/.*\S.*/).required().messages({
+        'string.pattern.base': 'O campo nome quando preenchido não pode ser encaminhado vazio.',
+        'any.required': 'O campo nome é obrigatório',
+        'any.empty': 'O campo nome é obrigatório',
+        'string.empty': 'O campo nome é obrigatório',
+
+    }),
+    descricao: joi.string().pattern(/.*\S.*/).messages({
+        'string.pattern.base': 'O campo descricao quando preenchido não pode ser encaminhado vazio.',
+        'string.base': 'O campo descrição deve ser um texto.'
+    }),
+    preco: joi.number().integer().positive().required().messages({
+        'number.base': 'O campo preço deve ser um número',
+        'any.required': 'O campo preço é obrigatório',
+        'number.positive': 'O campo preço deve ser um número positivo'
+    }),
+    imagem_url: joi.string().pattern(/.*\S.*/).messages({
+        'string.pattern.base': 'O campo url da imagem quando preenchido não pode ser encaminhado vazio.',
+        'string.base': 'O campo url da imagem deve ser um texto.'
+    }),
+    quant_estoque: joi.number().integer().positive().required().messages({
+        'number.base': 'O campo quantidade em estoque deve ser um número',
+        'any.required': 'O campo prquantidade em estoqueeço é obrigatório',
+        'number.positive': 'O campo quantidade em estoque deve ser um número positivo'
+    })
+})
+
+module.exports = schemaSabor;
