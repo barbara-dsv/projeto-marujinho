a93f652f3fa975ddd63e1faa2e7567c59ab500b8
Bárbara Lourenço Tavares <babisolua@gmail.com>
2025-05-26 19:05:24 -0300

    :sparkles:	feat:  add rotas de cadastrar sabor, atualizar sabor, listar todos pedido e listar pedido com id. adicionado '/admin' a url's que são acessadas somente por admin. E organizadas as importações.

--- src/rotas.js
+++ src/rotas.js
@@ -1,13 +1,24 @@
 const express = require('express');
+//controladores
 const cadastrarAdmin = require('./controladores/admin/cadastrarAdmin');
 const login = require('./controladores/admin/login')
+const cadastrarSabor = require('./controladores/admin/cadastrarSabor');
 const listarSabores = require('./controladores/sabores/listarSabores');
 const criarPedido = require('./controladores/pedidos/criarPedido');
-const validarRequisicao = require('./intermediadores/validarRequisicao')
+const validarRequisicao = require('./intermediadores/validarRequisicao');
+const atualizarSabor = require('./controladores/admin/atualizarSabor');
+const listarPedidos = require('./controladores/admin/listarPedidos');
+const listarPedido = require('./controladores/admin/listarPedido');
+
+//schemas
 const schemaAdmin = require('./schemas/schemaAdmin')
 const schemaLogin = require('./schemas/schemaLogin');
 const schemaPedido = require('./schemas/schemaPedido');
+const schemaSabor = require('./schemas/schemaSabor');
+const schemaAtualizarSabor = require('./schemas/schemaAtualizarSabor');
+
 const autenticacao = require('./intermediadores/autenticacao');
+
 const rotas = express();
 
 
@@ -15,11 +26,19 @@ rotas.get('/', (req, res) => {
     res.send('API Marujinho funcionando! 🌊🍦')
 });
 
-rotas.post('/usuario', validarRequisicao(schemaAdmin), cadastrarAdmin);
-rotas.post('/login', validarRequisicao(schemaLogin), login);
+rotas.post('/admin/usuario', validarRequisicao(schemaAdmin), cadastrarAdmin);
+rotas.post('/admin/login', validarRequisicao(schemaLogin), login);
 rotas.get('/sabores', listarSabores);
 rotas.post('/pedido', validarRequisicao(schemaPedido), criarPedido)
 
 rotas.use(autenticacao);
+
+rotas.post('/admin/sabores', validarRequisicao(schemaSabor), cadastrarSabor)
+rotas.put('/admin/sabor/:id', validarRequisicao(schemaAtualizarSabor), atualizarSabor)
+
+rotas.get('/admin/pedidos', listarPedidos)
+rotas.get('/admin/pedido/:id', listarPedido)
+
+//TODO:rotas.put('/admin/pedido/:id/status')*/
 
 module.exports = rotas;
