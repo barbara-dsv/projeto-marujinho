80b8d44757058606bd0081f809ee949bd64f5601
Bárbara Lourenço Tavares <babisolua@gmail.com>
2025-05-26 18:01:24 -0300

    :sparkles:	feat: add controlador de listar pedidos

---
+++ src/controladores/admin/listarPedidos.js
@@ -0,0 +1,13 @@
+const knex = require('../../bancoDeDados/config');
+
+const listarPedidos = async (req, res) => {
+    try {
+        const pedidos = await knex('pedidos').returning('*');
+
+        return res.status(200).json(pedidos)
+    } catch (error) {
+        return res.status(500).json({ mensagem: 'Error interno do servidor' })
+    }
+}
+
+module.exports = listarPedidos;
