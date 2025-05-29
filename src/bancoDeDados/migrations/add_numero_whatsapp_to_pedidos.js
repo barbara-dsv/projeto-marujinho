d1df6151adb2396f89862d5f2c15c41a4a73bb44
Bárbara Lourenço Tavares <babisolua@gmail.com>
2025-05-28 21:32:44 -0300

    :card_file_box:	raw: adicionado migration para inserir coluna de numero _whataspp a tabela 'pedidos'

---
+++ src/bancoDeDados/migrations/add_numero_whatsapp_to_pedidos.js
@@ -0,0 +1,11 @@
+exports.up = function (knex) {
+    return knex.schema.table("pedidos", function (table) {
+        table.string("numero_whatsapp", 20).nullable();
+    });
+};
+
+exports.down = function (knex) {
+    return knex.schema.table("pedidos", function (table) {
+        table.dropColumn("numero_whatsapp");
+    });
+};
