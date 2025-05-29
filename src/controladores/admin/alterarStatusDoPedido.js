c10eeabeb77586b9db981232272158e99ad2275b
Bárbara Lourenço Tavares <babisolua@gmail.com>
2025-05-29 17:38:27 -0300

    :sparkles:	feat: adicionada lógica no controlador de alterar status do pedido

--- src/controladores/admin/alterarStatusDoPedido.js
+++ src/controladores/admin/alterarStatusDoPedido.js
@@ -2,12 +2,29 @@
 const knex = require("../../bancoDeDados/config");
 
 const alterarStatusDoPedido = async (req, res) => {
-    const { id: idPedido } = req.params;
+    const { nome } = req.params;
+    const { status } = req.body;
 
     try {
-        //TODO 
+        const pedidosPorNome = await knex('pedidos')
+            .whereILike('nome_cliente', `%${nome}%`)
+            .orderBy('data_pedido', 'desc');
+
+        if (pedidosPorNome.length === 0) {
+            return res.status(404).json({ mensagem: "Nenhum pedido encontrado com esse nome." });
+        }
+
+        if (pedidosPorNome[0].status === "concluído") {
+            return res.status(404).json({ mensagem: "Não é possível alterar o status de um pedido concluído." });
+        }
+        const idPedido = pedidosPorNome[0].id
+
+        await knex('pedidos').where({ id: idPedido }).update({ status }).returning('*')
+
+        return res.status(200).json({ mensagem: "Sabor atualizado com sucesso." });
 
     } catch (error) {
+        console.log(error)
         return res.status(500).json({ mensagem: "Erro do servidor" })
     }
 };
