0e4d2f62fe4c96419bcf3513c1581e72ce43e665
Bárbara Lourenço Tavares <babisolua@gmail.com>
2025-05-29 17:36:07 -0300

    :sparkles:	feat: adicionado schema de validação de status de pedido para rota de alterar status

---
+++ src/schemas/schemaAlterarStatus.js
@@ -0,0 +1,13 @@
+const Joi = require('joi');
+
+const schemaStatus = Joi.object({
+    status: Joi.string()
+        .valid('pendente', 'em entrega', 'concluído')
+        .required()
+        .messages({
+            'any.only': 'Status inválido. Deve ser "pendente", "em entrega" ou "concluído".',
+            'any.required': 'O status é obrigatório.',
+        }),
+});
+
+module.exports = schemaStatus
