f3509949f9ac14cb9e9f74ba6d70b49599503d26
Bárbara Lourenço Tavares <babisolua@gmail.com>
2025-05-30 22:16:09 -0300

    :sparkles:	feat: adicionado função de validação de cep por bairro

---
+++ src/utils/validarCep.js
@@ -0,0 +1,27 @@
+const axios = require("axios");
+
+const validarCep = async (cep) => {
+    try {
+        const resposta = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+        const dados = resposta.data;
+
+        if (dados.erro) {
+            return { valido: false, mensagem: "CEP inválido" };
+        }
+
+        const bairro = dados.bairro?.toLowerCase();
+
+        if (!bairro.includes("mucuripe")) {
+            return {
+                valido: false,
+                mensagem: "No momento só fazemos entregas no bairro Mucuripe.",
+            };
+        }
+
+        return { valido: true };
+    } catch (error) {
+        return { valido: false, mensagem: "Erro ao validar o CEP" };
+    }
+}
+
+module.exports = validarCep;
